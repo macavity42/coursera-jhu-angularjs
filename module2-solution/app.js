47b05004e17605434e925cd5e5c1aff74579ebe3
Robert Davis <robdavis@usa.net>
2016-09-25 17:24:17 -0700

    First part working, correctly defined 2 controllers and service and able to buy items.

---
+++ module2-solution/app.js
@@ -0,0 +1,56 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+	var toBuy = this;
+	
+	toBuy.itemsToBuy = ShoppingListCheckOffService.getItemsToBuy();
+	
+	toBuy.buy = function(index) {
+		ShoppingListCheckOffService.buyItem(index);
+	}
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+	var alreadyBought = this;
+	
+	alreadyBought.itemsBought = ShoppingListCheckOffService.getItemsBought();
+}
+
+function ShoppingListCheckOffService() {
+	var service = this;
+	
+	var toBuy = [ 
+		{ name: "cookies", quantity: 10 },
+		{ name: "chips", quantity: 5 },
+		{ name: "sodas", quantity: 7 },
+		{ name: "crackers", quantity: 8 },
+		{ name: "apples", quantity: 2 }
+	];
+	
+	var bought = [];
+	
+	service.getItemsToBuy = function () {
+		return toBuy;
+	}
+
+	service.getItemsBought = function () {
+		return bought;
+	}
+	
+	service.buyItem = function (index) {
+		bought.push(toBuy[index]);
+		toBuy.splice(index, 1);
+	}
+}
+
+
+})();
